6008f36128011bdfa561ba0979fe29d37903797b
Artem Novichkov <novichkoff93@gmail.com>
2015-04-11 13:02:06 +0600

    Initial Commit

---
+++ WeatherToday/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WeatherToday
+//
+//  Created by Артем on 11.04.15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
